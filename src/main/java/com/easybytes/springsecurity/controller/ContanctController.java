3fcd84727478b3b86b0ba582bac8a548f964aa95
Darlington Olelewe <darlington.olelewe@decagon.dev>
2022-05-07 04:58:21 +0100

    setup

---
+++ src/main/java/com/easybytes/springsecurity/controller/ContanctController.java
@@ -0,0 +1,4 @@
+package com.easybytes.springsecurity.controller;
+
+public class ContanctController {
+}
---
+++ src/main/java/com/easybytes/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.easybytes.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "welcome to springSecurity";
+    }
+}
